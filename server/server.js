659516f17d68e689cc7eb895fac808e2d544a11f
Лим Николай Андреевич <Lim.NA@berezapf.bmk.by>
2022-09-21 10:48:52 +0300

    +post routing at server side

--- server/server.js
+++ server/server.js
@@ -13,17 +13,21 @@ const options = {
 server.use(express.json())
 server.use(cors(options))
 
-
-let data = fs.readFileSync('data.json')
-
 server.get('/', (req, res)=>{
-        res.status(200).json(JSON.parse(data))
+        res.status(200).json(JSON.parse(fs.readFileSync('data.json')))
 })
+
 server.post('/', (req, res)=>{
-        fs.writeFileSync('data.json', JSON.stringify(req.body))
+        fs.readFile('data.json', 'utf8', (err, data) => {
+                let json = JSON.parse(data)
+                json.push(req.body)
+                fs.writeFile("data.json", JSON.stringify(json),
+                (err, result)=>{
+                        if(err) console.log('error', err);
+                      })
+            });
+            
         res.status(200).json(JSON.parse(data))
 })
+
 server.listen(PORT, ()=> console.log("SERVER STARTED"))
-
-
-
